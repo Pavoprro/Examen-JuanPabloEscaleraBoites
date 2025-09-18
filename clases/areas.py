a956ea289d9b4b56a4d190f4ad41c1b790180ba0
Pavo pro <194533-3@iberoleon.edu.mx>
2025-09-18 13:47:42 -0600

    Agregar funcion para circulo

--- clases/areas.py
+++ clases/areas.py
@@ -1,10 +1,18 @@
+import math
+
 class Areas():
     def leer_numero(self):
         self.base= float(input("Ingresa la base de la figura "))
         self.altura = float(input("Ingrese la altura de la figura "))
+
+    def leer_radio(self):
+        self.radio= float(input("Ingrese el radio del circulo "))
 
     def area_triangulo(self):
         return ((self.base*self.altura)/2)
 
     def area_rectangulo(self):
         return (self.base*self.altura)
+    
+    def area_circulo(self):
+        return (math.pi * (self.radio ** 2))
--- main.py
+++ main.py
@@ -5,3 +5,6 @@ if __name__ == "__main__":
     calc.leer_numero()
     print("triangulo", calc.area_triangulo())
     print("rectangulo", calc.area_rectangulo())
+
+    calc.leer_radio()
+    print("circulo", calc.area_circulo())
